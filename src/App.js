6baed82aa34c97a05a5c89a912fb009aab6e9e5a
Menza <menzamenza90@gmail.com>
2024-08-30 22:34:55 +0000

    coms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { PostProvider } from './contexts/PostContext';
+import PostList from './components/PostList';
+import PostDetails from './components/PostDetails';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PostProvider>
+      <Router>
+        <div className="App">
+          <h1>Blog Posts</h1>
+          <Routes>
+            <Route path="/" element={<PostList />} />
+            <Route path="/post/:id" element={<PostDetails />} />
+          </Routes>
+        </div>
+      </Router>
+    </PostProvider>
   );
 }
 
